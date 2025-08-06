1201119d697f824026c6889820f78b26089681d5
HL-Yang <yhliang916@163.com>
2025-08-06 19:44:25 +1000

    unfinished Homepage

---
+++ frontend/src/components/HomePage.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+const HomePage = ({username}:{username:string}) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const text: {
+        [key:string]:{
+            home: string;
+            settings: string;
+            download: string;
+        }
+        } = {
+            zh:{
+                home:"主页",
+                settings:"设置",
+                download: "下载",
+            },
+            en:{
+                home:"Homepage",
+                settings: "Setting",  
+                download: "Download",  
+            }
+        }
+    const toggleSideBar = () =>{
+        setIsExpanded(!isExpanded);
+    }
+
+    return 
+};
+
+export default HomePage;
